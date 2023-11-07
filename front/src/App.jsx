e8d69e073e717bd980da6bc7e9ee24364723dfc1
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-07 19:33:57 -0400

    Render Nav component from App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Nav from './components/Nav'; 
 import Cards from './components/Cards.jsx';
 import characters from './data.js';
 
 function App() {
    return (
       <div className='App'>
+         <Nav />
          <Cards characters={characters} />
       </div>
    );
