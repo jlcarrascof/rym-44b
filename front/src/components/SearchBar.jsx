c2788bc076a17ed15c7f33294477ee966ee6da29
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-06 23:06:26 -0400

    Update project from create-react-app to vite

---
+++ front/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+export default function SearchBar({onSearch}) {
+   console.log(onSearch);
+   return (
+      <div>
+         <input type='search' />
+         <button onClick={() => onSearch('ID del personaje')}>
+            Agregar
+         </button> 
+      </div>
+   );
+}
