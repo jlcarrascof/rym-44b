2d3c7e01042a13d3954d0c09aea78e4e53989c8f
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-07 19:29:24 -0400

    Delete Card component from App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,22 +1,11 @@
 import './App.css';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-import characters, { Rick } from './data.js';
+import characters from './data.js';
 
 function App() {
    return (
       <div className='App'>
          <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
       </div>
    );
 }
