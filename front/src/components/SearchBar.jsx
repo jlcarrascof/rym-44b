adc29e2904fc5da2855468730dcf51ebe3c37cf7
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-07 23:12:07 -0400

    Put an ID param in our searchbar

--- front/src/components/SearchBar.jsx
+++ front/src/components/SearchBar.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 export default function SearchBar({onSearch}) {
-   // console.log(onSearch);
+   
+   const [id, setID] = useState('');
+   
+   const handleChange = (evento) => { 
+      console.log(evento);
+   }
+   
    return (
       <div>
-         <input type='search' />
+         <input type='search' onChange={handleChange}  value={id} />
          <button onClick={() => onSearch()}>
             Agregar
          </button> 
