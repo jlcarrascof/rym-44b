e257b059efa985af8125f33784561154cf8982f0
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-08 10:38:34 -0400

    Add my personal API KEY

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 import Nav from './components/Nav'; 
 import Cards from './components/Cards.jsx';
 
@@ -8,6 +9,7 @@ function App() {
    
    const [characters, setCharacters] = useState([]);
    
+   /*
    const example = {
       id: 1,
       name: 'Rick Sanchez',
@@ -20,9 +22,13 @@ function App() {
       },
       image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
    };
+   */
+
+   const API_KEY = 'pi-javierjmartinezf';
 
    function onSearch(id) {
-      setCharacters(oldChars => [...oldChars, example]);
+      // setCharacters(oldChars => [...oldChars, example]);
+      axios(`https://rym2.up.railway.app/api/character/{id}?key=${API_KEY}`)
    }
 
    return (
