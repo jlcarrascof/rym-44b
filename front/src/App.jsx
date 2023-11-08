45cc66459bef02142aa01d0eaee46f1cc8460453
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-08 17:31:38 -0400

    Fix and test add characters functionalities

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -25,16 +25,13 @@ function App() {
    */
 
    const API_KEY = 'pi-javierjmartinezf';
-
-   /*
-
-      function onSearch(id){
+   
+   function onSearch(id) {
       if(!id) alert('Ingresa por favor un ID')
       if(characters.find(char => char.id === parseInt(id))) return alert (`Ya existe el personaje con ese id ${id}`)
 
       axios(`https://rym2.up.railway.app/api/character/${id}?key=${API_KEY}`)
       .then(({data})=>{
-         // console.log(data);
          if(data.name){
             setCharacters(oldChars => [...oldChars,data])
          }
@@ -44,44 +41,6 @@ function App() {
       })
       .catch(err => console.log(err))
    }
-
-   */
-
-   function onSearch(id) {
-      /*
-      if (!id) alert('Por favor, ingrese un ID válido');
-
-      if (characters.find(char => char.id === parseInt(id))) 
-      return alert('El personaje ya se encuentra en la lista');
-      
-
-      axios(`https://rym2.up.railway.app/api/character/${id}?key=${API_KEY}`)
-      .then(({data}) => {
-         if (data.name) {
-            setCharacters(oldChars => [...oldChars, data]);
-         }
-         else {
-            alert('No se encontró el personaje');
-         }   
-      })
-      .catch(err => console.log(err));
-      */
-      if(!id) alert('Ingresa por favor un ID')
-      if(characters.find(char => char.id === parseInt(id))) return alert (`Ya existe el personaje con ese id ${id}`)
-
-      axios(`https://rym2.up.railway.app/api/character/${id}?key=${API_KEY}`)
-      .then(({data})=>{
-         // console.log(data);
-         if(data.name){
-            setCharacters(oldChars => [...oldChars,data])
-         }
-         else {
-           alert( 'No hay personajes con ese ID')
-         }
-      })
-      .catch(err => console.log(err))
-
-      }
 
    return (
       <div className='App'>
