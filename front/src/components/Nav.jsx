266593c8372b9ec786519c82e619ec3a6f15ba67
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-17 02:33:46 -0400

    Add navigation links to Nav component using buttons

--- front/src/components/Nav.jsx
+++ front/src/components/Nav.jsx
@@ -1,8 +1,15 @@
 import SearchBar from './SearchBar.jsx';
+import { Link } from 'react-router-dom';
 
 function Nav({ onSearch }) {
 
     return <div>
+        <Link to='/home'>
+            <button>Home</button>
+        </Link>
+        <Link to='/about'>
+            <button>About</button>
+        </Link>
         <SearchBar onSearch={onSearch} />
     </div>
 }
