4e4b1d746808345a29f20aff03af81a4a384856c
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-17 02:26:25 -0400

    Create About component

---
+++ front/src/components/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+
+    return <div>
+        <h1>Acerca de la app de Rick & Morty</h1>
+        <h4>Esta app fue creada por Javier Martinez</h4>
+    </div>
+}
+
+export default About;
