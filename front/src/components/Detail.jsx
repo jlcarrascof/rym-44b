c22f23d0e6ca5f7a0ba1aaac30161ed0d5dcdbb2
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-18 11:35:12 -0400

    Begin to code Detail component using axios and useEffect state

--- front/src/components/Detail.jsx
+++ front/src/components/Detail.jsx
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const API_KEY = 'pi-javierjmartinezf';
+
 function Detail() {
+
+    const { id } = useParams();
+    const [character, setCharacter] = useState({
+
+    });
+
+    useEffect(() => {
+        axios(`https://rym2.up.railway.app/api/character/${id}?key=${API_KEY}`)
+            .then(response => setCharacter(response.data))
+    }, []);
 
     return <div>
         Estoy en el Detail
