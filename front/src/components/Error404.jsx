6572d5603a9b8ff95e20f726b1d4a19de503ab90
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-18 13:18:21 -0400

    Update Error404 page

--- front/src/components/Error404.jsx
+++ front/src/components/Error404.jsx
@@ -2,7 +2,17 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 function Error404() { 
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        SetTimeout(() => {
+            navigate("/home");
+        }, 3000);
+    }, []);
+
+    return <div>
+        404 - Page not found
+    </div>;
 }
 
 export default Error404;
