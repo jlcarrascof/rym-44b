bdf28d7121c7f0bf2737fe393231566c2da9b3bb
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-20 22:57:27 -0400

    Finish requirement 1/6

--- front/src/components/Form.jsx
+++ front/src/components/Form.jsx
@@ -1,7 +1,22 @@
 function Form() {
 
     return <div>
-        
+        <form>
+            <label htmlFor="email">
+                Email:
+                <input type="text" />
+            </label>
+
+            <label htmlFor="password">
+                Password:
+                <input type="text" />
+            </label>
+
+            <button>
+                Submit
+            </button>
+
+        </form>           
     </div>
 }
 
