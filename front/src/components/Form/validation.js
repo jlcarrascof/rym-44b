974705933947c63e3deb583e19dc47c51ddd5441
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-21 19:13:18 -0400

    Coding Javascript validation version in comments

--- front/src/components/Form/validation.js
+++ front/src/components/Form/validation.js
@@ -8,6 +8,15 @@ function validation(data) {
     if (!regexEmail.test(data.email)) errors.email = 'El correo electrónico no es válido';
     if (!exLongChar.test(data.email)) errors.email = 'La longitud debe tener entre 1 y 35 caracteres';
     if (!regexPassword.test(data.password)) errors.password = 'La contraseña debe tener entre 6 y 10 caracteres y al menos un número';
+
+    // Option 2 (using Javascript logic)
+    if (data.email === '') errors.email = 'El correo electrónico es requerido';
+    if (data.email.length > 35) errors.email = 'La longitud debe tener entre 1 y 35 caracteres';
+    if (data.password === '') errors.password = 'La contraseña es requerida';
+    if (data.password.length < 6) errors.password = 'La contraseña debe tener entre 6 y 10 caracteres y al menos un número';
+
+
+    return errors;
 }
 
 export default validation;
