7d9084f3cb910145fd3f61234964909606d4642a
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-21 19:08:47 -0400

    Validate password length

--- front/src/components/Form/validation.js
+++ front/src/components/Form/validation.js
@@ -7,6 +7,7 @@ function validation(data) {
 
     if (!regexEmail.test(data.email)) errors.email = 'El correo electrónico no es válido';
     if (!exLongChar.test(data.email)) errors.email = 'La longitud debe tener entre 1 y 35 caracteres';
+    if (!regexPassword.test(data.password)) errors.password = 'La contraseña debe tener entre 6 y 10 caracteres y al menos un número';
 }
 
 export default validation;
