259301b6b53d4eaf0a5135fd26d5f6976f046848
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-21 17:55:15 -0400

    Validate email

--- front/src/components/Form/validation.js
+++ front/src/components/Form/validation.js
@@ -2,3 +2,12 @@ const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const exLongChar = /^(?=.{1,35}$).+/;
 const regexPassword = /^(?=.*\d)[0-9a-zA-Z]{6,10}$/
 
+function validation(data) {
+    let errors = {};
+
+    if (!regexEmail.test(data.email)) errors.email = 'El correo electrónico no es válido';
+    if (!exLongChar.test(data.name)) errors.name = 'El nombre no es válido';
+
+}
+
+export default validation;
