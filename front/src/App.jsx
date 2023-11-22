201606bfecc4bb528a486af3f7ef784f23a38f41
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-21 22:13:26 -0400

    Finish the requirement 5/6

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -71,7 +71,7 @@ function App() {
       <div className='App'>
          { pathname !== '/' && <Nav onSearch={onSearch} /> }
          <Routes>
-            <Route path='/' element={<Form />} />
+            <Route path='/' element={<Form login={login} />} />
             <Route path='/home' element={<Cards characters={characters} onClose={onClose} />} />
             <Route path='/about' element={<About />} />
             <Route path='/detail/:id' element={<Detail />} />
