c5045b2a041b054458297baec98daaefb8385a62
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-11-30 22:43:55 -0400

    Create our reducer

--- front/src/redux/reducer.js
+++ front/src/redux/reducer.js
@@ -1,4 +1,29 @@
+import { ADD_FAV, REMOVE_FAV } from './actions/actionsTypes';
+
 function reducer() {
+
+    const initialState = {
+        myFavorites: []
+    };
+    
+    function reducer(state = initialState, { type, payload }) {
+        switch (type) {
+        case ADD_FAV:
+            return {
+            ...state,
+            myFavorites: [...state.myFavorites, payload]  
+            };
+        case REMOVE_FAV:
+            return {
+            ...state,
+            myFavorites: state.myFavorites.filter(char => char.id !== Number(payload))
+            };
+        default:
+            return {...state};
+        }
+    }
+    
+    return reducer;
 
 }
 
