66a5c82b171d488c1c1f0ab2eb5538ccd5276703
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-01 19:20:33 -0400

    Coding and testing my Favorites component

--- front/src/components/Favorites.jsx
+++ front/src/components/Favorites.jsx
@@ -1,7 +1,30 @@
-function Favorites() {
-  return (
-    <div>Favorites</div>
-  )
+import { connect } from 'react-redux'
+import Card from './Card'
+
+function Favorites({ myFavorites }) {
+    return <div>
+            <h1>Favorites</h1>
+            {
+                myFavorites.map((char) => { 
+                    return <Card 
+                    key={char.id}
+                    id={char.id} 
+                    name={char.name}
+                    status={char.state}
+                    species={char.species}
+                    gender={char.gender}
+                    origin={char.origin}
+                    image={char.image}
+                     />   
+                })
+            }
+        </div>
 }
 
-export default Favorites
+function mapStateToProps(state) {
+   return {
+      myFavorites: state.myFavorites
+   }
+}
+
+export default connect(mapStateToProps)(Favorites)
