976b1755e0b406b0b535a59f93592ed517a8e11d
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-09 18:06:50 -0400

    Fix actions

--- front/src/redux/actions/actions.js
+++ front/src/redux/actions/actions.js
@@ -6,27 +6,27 @@ export function addFav(character) {
   return { 
     type: ADD_FAV, 
     payload: character
-  };
+  }
 }
 
 export function removeFav(id) {
   return { 
     type: REMOVE_FAV, 
     payload: id 
-  };
+  }
 }
 
 export function filterCards(gender) {
   return {
     type: FILTER,
     payload: gender
-  };  
+  } 
 }
 
 export function orderCards(payload) {
   return {
     type: ORDER,
     payload
-  };
+  }
 }
 
