86a02e575ae287f2b6038025e0ce98e074edf81e
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-12-10 01:58:22 -0400

    Fix error on my favorites component (it was the wrong call in onChange)

--- front/src/components/Favorites.jsx
+++ front/src/components/Favorites.jsx
@@ -5,7 +5,7 @@ import Card from './Card'
 function Favorites() {
     const dispatch = useDispatch()
     const myFavorites = useSelector(state => state.myFavorites)
-    console.log(myFavorites)
+    // console.log(myFavorites)
 
     function handleOrder(evento) {
         dispatch(orderCards(evento.target.value))
@@ -18,14 +18,14 @@ function Favorites() {
     return <div>
 
         <div>
-            <select name="filter" onChange={filterCards}>
+            <select name="filter" onChange={handleFilter}>
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
                 <option value="Genderless">Genderless</option>
                 <option value="unknown">Unknown</option> 
             </select>
 
-            <select name="order" onChange={orderCards}>
+            <select name="order" onChange={handleOrder}>
                 <option value="ascendente">Ascendente</option>
                 <option value="descendente">Descendente</option>
             </select>        
