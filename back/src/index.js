47fa4e471067202dd0138f33323dfe1d91eeb798
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-13 12:33:25 -0400

    Saving when I can't find the id

--- back/src/index.js
+++ back/src/index.js
@@ -1,13 +1,20 @@
 const http = require('http');
+const data = require('./utils/data');
 
 http.createServer((req, res) => {
     const { url } = req;
     // console.log(url);
     res.setHeader('Access-Control-Allow-Origin', '*');
     if (url.includes('/rickandmorty/character')) {
-        // res.end('Estoy en la ruta');
         const id = url.split('/').at(-1);
-        console.log(id);
+        const character = data.find((char) => char.id === parseInt(id));
+        // console.log(character);
+        if (character) {
+
+        } else {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            return res.end(JSON.stringify({ error: 'Character not found' }));
+        }    
     }
 }).listen(3001, "localhost");
 
