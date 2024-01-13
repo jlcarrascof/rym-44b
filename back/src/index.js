9b89cf18116692b0813484375a44a0a93ab11395
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-13 09:30:42 -0400

    Save the last character in the path as 'id'.

--- back/src/index.js
+++ back/src/index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-   
+    const { url } = req;
+    // console.log(url);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (url.includes('/rickandmorty/character')) {
+        // res.end('Estoy en la ruta');
+        const id = url.split('/').at(-1);
+        console.log(id);
+    }
 }).listen(3001, "localhost");
 
