2ffee619011316a41f394c115e3c25cf8ff97215
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-20 23:05:53 -0400

    Improving the code of getCharById.js with security options

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
-const { URL, API_KEY } = process.env;
+const axios = require('axios');
+const { urlCharacterId } = require('../utils/reusable');
+
 function getCharById(req, res) {
-    
+    axios(urlCharacterId(id))    
 }
 
 module.exports = getCharById;
---
+++ back/src/utils/reusable.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const { URL, API_KEY, QUERY_KEY } = process.env;
+const urlCharacterId = (id) => `${URL}/${id}?${QUERY_KEY}=${API_KEY}`;
+
+module.exports = urlCharacterId;
