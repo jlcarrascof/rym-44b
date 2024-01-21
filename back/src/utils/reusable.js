e3970a97c64df7bdaec73cad6d818dec0f628407
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-21 18:40:49 -0400

    Export urlCharacterId using data from .env file

--- back/src/utils/reusable.js
+++ back/src/utils/reusable.js
@@ -2,4 +2,6 @@ require('dotenv').config();
 const { URL, API_KEY, QUERY_KEY } = process.env;
 const urlCharacterId = (id) => `${URL}/${id}?${QUERY_KEY}=${API_KEY}`;
 
-module.exports = urlCharacterId;
+module.exports = {
+    urlCharacterId
+};
