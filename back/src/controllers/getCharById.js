7d141e0b71feea5dbe8d981ddd79596d09f84cd7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-23 11:56:43 -0400

    Implementation of function to get character by ID

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -3,8 +3,15 @@ const { urlCharacterId } = require('../utils/reusable');
 
 function getCharById(res, id) {
     axios(urlCharacterId(id))
-    .then((res) => console.log(res)) 
+    .then((res) => res.data)
+    .then((character) => { 
+        console.log(character) 
+    }) 
     .catch((err) => console.log(err));        
 }
+
+// then retorna una nueva promesa.
+// como?
+// map retorna un nuevo arreglo? porque? porque quien creo el metodo asi lo definio.
 
 module.exports = getCharById;
