c509ce1f7e18a86fcfdb11d6e668d148cb3dcf84
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-08-04 19:33:34 -0400

    feat(redux): refactor store configuration and create favSlice using Redux Toolkit createSlice

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -1,8 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-import reducer from "./reducer";
+import { configureStore } from '@reduxjs/toolkit';
+import favReducer from './favSlice';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = configureStore({
+  reducer: {
+    favorites: favReducer,
+  },
+});
 
 export default store;
