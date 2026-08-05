5d67479096c3e0bfa17ec190952f4bbb9b2d45b0
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-08-04 21:52:06 -0400

    refactor(cards): modernize Card component with useDispatch/useSelector hooks and async favorite actions

--- front/src/components/cards/Cards.jsx
+++ front/src/components/cards/Cards.jsx
@@ -1,23 +1,22 @@
-import Card from '../card/Card';
+import Card from '../Card/Card';
 import style from './Cards.module.css';
 
 export default function Cards({ characters, onClose }) {
-   return (
-   <div className={style.container}>
-      {
-         characters.map((char) => 
-            (<Card
-               key={char.id}
-               id={char.id} 
-               name={char.name}
-               status={char.status}
-               species={char.species}
-               gender={char.gender}
-               origin={char.origin.name}
-               image={char.image}
-               onClose={onClose}   
-            />)
-         )
-      }
-   </div>);
+  return (
+    <div className={style.container}>
+      {characters && characters.map((char) => (
+        <Card
+          key={char.id}
+          id={char.id}
+          name={char.name}
+          status={char.status}
+          species={char.species}
+          gender={char.gender}
+          origin={typeof char.origin === 'object' ? char.origin.name : char.origin}
+          image={char.image}
+          onClose={onClose}
+        />
+      ))}
+    </div>
+  );
 }
