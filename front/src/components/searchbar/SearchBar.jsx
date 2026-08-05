299f5ff523f629c9bd5fe30201a6f0b219829cb9
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-08-04 22:08:27 -0400

    feat(searchbar): add Random character button and update input labels to English

--- front/src/components/searchbar/SearchBar.jsx
+++ front/src/components/searchbar/SearchBar.jsx
@@ -1,26 +1,37 @@
 import { useState } from 'react';
 import { Btn, DivSearch, Input } from './SearchStyled';  
 
-export default function SearchBar({onSearch}) {
-   
-   const [id, setID] = useState('');
-   
-   const handleChange = (evento) => { 
-      console.log(evento);
-      setID(evento.target.value);
-   }
-   
-   const search = () => {  
+export default function SearchBar({ onSearch }) {
+  const [id, setID] = useState('');
+
+  const handleChange = (evento) => {
+    setID(evento.target.value);
+  };
+
+  const search = () => {
+    if (id) {
       onSearch(id);
       setID('');
-   }
+    }
+  };
 
-   return (
-      <DivSearch>
-         <Input type='search' onChange={handleChange}  value={id} />
-         <Btn onClick={search}>
-            Agregar
-         </Btn> 
-      </DivSearch>
-   );
+  const handleRandom = () => {
+    const randomId = Math.floor(Math.random() * 826) + 1;
+    onSearch(randomId);
+  };
+
+  return (
+    <DivSearch>
+      <Input
+        type="search"
+        placeholder="Enter ID (1-826)..."
+        onChange={handleChange}
+        value={id}
+      />
+      <Btn onClick={search}>Add</Btn>
+      <Btn onClick={handleRandom} style={{ marginLeft: '5px' }}>
+        Random 🎲
+      </Btn>
+    </DivSearch>
+  );
 }
