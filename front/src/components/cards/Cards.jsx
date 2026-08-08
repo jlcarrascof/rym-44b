81bf62983cc8c4edfd8c87f0f9a241a46db5dad2
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-08-07 21:51:56 -0400

    style(home): add Hero Header title and subtitle to Home page layout

--- front/src/components/cards/Cards.jsx
+++ front/src/components/cards/Cards.jsx
@@ -4,21 +4,33 @@ import style from './Cards.module.css';
 
 export default function Cards({ characters, onClose }) {
   return (
-    <div className={style.container}>
-      {characters &&
-        characters.map((char) => (
-          <Card
-            key={char.id}
-            id={char.id}
-            name={char.name}
-            status={char.status}
-            species={char.species}
-            gender={char.gender}
-            origin={typeof char.origin === 'object' ? char.origin.name : char.origin}
-            image={char.image}
-            onClose={onClose}
-          />
-        ))}
+    <div className={style.wrapper}>
+      <h1 className={style.heroTitle}>Rick & Morty Multiverse Explorer</h1>
+      <p className={style.heroSubtitle}>
+        Explore dimensions, search by ID, and save your favorite characters.
+      </p>
+
+      <div className={style.container}>
+        {characters && characters.length > 0 ? (
+          characters.map((char) => (
+            <Card
+              key={char.id}
+              id={char.id}
+              name={char.name}
+              status={char.status}
+              species={char.species}
+              gender={char.gender}
+              origin={typeof char.origin === 'object' ? char.origin.name : char.origin}
+              image={char.image}
+              onClose={onClose}
+            />
+          ))
+        ) : (
+          <p className={style.emptyState}>
+            No characters added yet. Enter an ID (1-826) above or click Random 🎲!
+          </p>
+        )}
+      </div>
     </div>
   );
 }
